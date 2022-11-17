43bb1f67e5e797b183aa93e2613f776d615d09d0
alejandro.pienda <alejandropi301196@gmail.com>
2022-11-18 00:27:01 +0100

    Agrego controller y servicios de mongo

--- Plants.Services/obj/Debug/net6.0/Plants.Services.AssemblyInfo.cs
+++ Plants.Services/obj/Debug/net6.0/Plants.Services.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("73b46e32-06e3-46ff-ba3b-a8dd3956cb92")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Plants.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
