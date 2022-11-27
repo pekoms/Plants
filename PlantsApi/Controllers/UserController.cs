094e3f7ef4cf26872cf7967b5efec9ad2d26f515
alejandro.pienda <alejandropi301196@gmail.com>
2022-11-27 18:38:25 +0100

    Setting up controller

--- PlantsApi/Controllers/UserController.cs
+++ PlantsApi/Controllers/UserController.cs
@@ -21,7 +21,7 @@ namespace Plants.Api.Controllers
         }
 
         // GET: UserController
-        [HttpGet("user",Name = "GetAllUsers")]
+        [HttpGet(Name = "GetAllUsers")]
         public async Task<ActionResult<List<User>>> Get(CancellationToken cancellationToken)
         {
             var users = await _userService.GetAllUsers();
@@ -30,7 +30,7 @@ namespace Plants.Api.Controllers
         }
 
         //GET: UserController/Details/5
-        [HttpGet("user/{id}",Name = "GetDetailedUser")]
+        [HttpGet("{id}",Name = "GetDetailedUser")]
         public async Task<ActionResult<User>> GetDetailedUser(string id, CancellationToken cancellationToken)
         {
             var user = await _userService.Get(id); ;
@@ -40,7 +40,7 @@ namespace Plants.Api.Controllers
 
         // POST: UserController/Create
 
-        [HttpPost("user",Name ="CreateUser")]
+        [HttpPost(Name ="CreateUser")]
         public async Task<ActionResult> CreateUser([FromBody] User newUser,CancellationToken cancellationToken)
         {
             await _userService.Create(newUser);
@@ -48,7 +48,7 @@ namespace Plants.Api.Controllers
         }
 
         // POST: UserController/Edit/5
-        [HttpPut("user/{id}",Name ="UpdateDetailedUser")]
+        [HttpPut("{id}",Name ="UpdateDetailedUser")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> UpdateDetailedUser(string id, [FromBody] User user, CancellationToken cancellationToken)
         {
@@ -58,7 +58,7 @@ namespace Plants.Api.Controllers
         }
 
         // Delete: UserController/Delete/5
-        [HttpDelete("user/{id}", Name = "DeleteDetailedUser")]
+        [HttpDelete("{id}", Name = "DeleteDetailedUser")]
         public async Task<ActionResult> Delete(string id)
         {
              await _userService.Remove(id);
